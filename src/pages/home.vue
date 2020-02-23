19ead968e6d5ba0f37fbbaa026cd658c8c5f73fe
xuzetings <xuzeting@xuzetingdeMacBook-Pro.local>
2020-02-24 04:53:11 +0800

    imgs

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,21 +1,18 @@
 <template>
-    <div>
-        home
-        <nav-header></nav-header>
-        <router-view></router-view>
-        <nav-footer></nav-footer>
-    </div>
+  <div>
+    <nav-header></nav-header>
+    <router-view></router-view>
+    <nav-footer></nav-footer>
+  </div>
 </template>
 <script>
-import NavHeader from './../components/NavHeader' 
-import NavFooter from './../components/NavFooter'
+import NavHeader from "./../components/NavHeader";
+import NavFooter from "./../components/NavFooter";
 export default {
-    
-    name:'nav-home',
-    components:{
-        NavHeader,
-        NavFooter
-    }
-    
-}
+  name: "nav-home",
+  components: {
+    NavHeader,
+    NavFooter
+  }
+};
 </script>
