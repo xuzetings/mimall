3b988184ec0c055aec661349f2bc2eacd98488e4
xuzetings <xuzeting@xuzetingdeMacBook-Pro.local>
2020-02-23 22:07:08 +0800

    axios 初体验

--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
 import Vue from 'vue'
-import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import App from './App.vue'
 
+axios.defaults.baseURL='/api';
+axios.defaults.timeout=8000;//超时拦截
+
+//接口错误拦截
+axios.interceptors.response.use(function(response){
+  let res = response.data;
+  if(res.status == 0){//状态码0代表成功
+    return res.data;
+  }else if(res.status == 10){//状态码10代表失败
+    window.location.href = '/#/login';
+  }else{
+    alert(res.msg);
+  }
+})
+
+
+Vue.use(VueAxios,axios)
 Vue.config.productionTip = false
 
 new Vue({
