7086325a97c88af23153e4d1192fed6f0c189948
xuzetings <xuzeting@xuzetingdeMacBook-Pro.local>
2020-02-27 22:34:11 +0800

    ServiceBar纠正

--- src/components/ServiceBar.vue
+++ src/components/ServiceBar.vue
@@ -24,5 +24,40 @@ export default {
 };
 </script>
 
+
 <style lang="scss">
+  @import './../assets/sass/mixin';
+  .service{
+    padding:33px 0;
+    color:#666666;
+    font-size:16px;
+    li{
+      display:inline-block;
+      width:24.9%;
+      text-align:center;
+      border-right:1px solid #E5E5E5;
+      span{
+        display:inline-block;
+        width:20px;
+        height:20px;
+        vertical-align: middle;
+        margin-right:8px;
+      }
+      .icon-setting{
+        @include bgImg(20px,20px,'/imgs/icon-setting.png');
+      }
+      .icon-7day{
+        @include bgImg(20px,20px,'/imgs/icon-7day.png');
+      }
+      .icon-15day{
+        @include bgImg(20px,20px,'/imgs/icon-15day.png');
+      }
+      .icon-post{
+        @include bgImg(20px,20px,'/imgs/icon-post.png');
+      }
+    }
+
+  }
 </style>
+
+
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,47 +1,67 @@
 <template>
-  <div>
+  <div class="index">
+    <div class="container">
+      <div class="swiper-box">
+        <swiper :options="swiperOption">
+          <!-- slides -->
+          <swiper-slide v-for="(item,index) in slideList" :key="index">
+            <a v-bind:href="'/#/product/'+item.id">
+              <img :src="item.img" alt />
+            </a>
+          </swiper-slide>
+          <!-- Optional controls -->
+          <div class="swiper-pagination" slot="pagination"></div>
+          <div class="swiper-button-prev" slot="button-prev"></div>
+          <div class="swiper-button-next" slot="button-next"></div>
+        </swiper>
+      </div>
+      <div class="ads-box"></div>
+      <div class="banner"></div>
+      <div class="product-box"></div>
+    </div>
     <service-bar></service-bar>
   </div>
 </template>
 <script>
+import "swiper/dist/css/swiper.css";
 import ServiceBar from "./../components/ServiceBar";
+import { swiper, swiperSlide } from "vue-awesome-swiper";
 export default {
   name: "index",
   components: {
+    swiper,
+    swiperSlide,
     ServiceBar
+  },
+  data() {
+    return {
+      swiperOption: {},
+      slideList: [
+        {
+          id: "42",
+          img: "/imgs/slider/slide-1.jpg"
+        },
+        {
+          id: "45",
+          img: "/imgs/slider/slide-2.jpg"
+        },
+        {
+          id: "46",
+          img: "/imgs/slider/slide-3.jpg"
+        },
+        {
+          id: "42",
+          img: "/imgs/slider/slide-4.jpg"
+        },
+        {
+          id: "42",
+          img: "/imgs/slider/slide-5.jpg"
+        }
+      ]
+    };
   }
 };
 </script>
 <style lang="scss">
-@import "./../assets/sass/mixin";
-.service {
-  padding: 33px 0;
-  color: #666666;
-  font-size: 16px;
-  li {
-    display: inline-block;
-    width: 24.9%;
-    text-align: center;
-    border-right: 1px solid #e5e5e5;
-    span {
-      display: inline-block;
-      width: 20px;
-      height: 20px;
-      vertical-align: middle;
-      margin-right: 8px;
-    }
-    .icon-setting {
-      @include bgImg(20px, 20px, "/imgs/icon-setting.png");
-    }
-    .icon-7day {
-      @include bgImg(20px, 20px, "/imgs/icon-7day.png");
-    }
-    .icon-15day {
-      @include bgImg(20px, 20px, "/imgs/icon-15day.png");
-    }
-    .icon-post {
-      @include bgImg(20px, 20px, "/imgs/icon-post.png");
-    }
-  }
-}
+  
 </style>
