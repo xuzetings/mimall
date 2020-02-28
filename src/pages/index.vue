fd13a5a6961788bc0dd0d5b0aa3d9d4fa01eeb92
xuzetings <xuzeting@xuzetingdeMacBook-Pro.local>
2020-02-28 17:17:50 +0800

    banner and list-box

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -52,42 +52,43 @@
           <div class="swiper-button-next" slot="button-next"></div>
         </swiper>
       </div>
-      <!-- <div class="ads-box">
+      <div class="ads-box">
         <a v-bind:href="'/#/product/'+item.id" v-for="(item,index) in adsList" v-bind:key="index">
-          <img v-lazy="item.img" alt="">
+          <img :src="item.img" alt />
         </a>
       </div>
       <div class="banner">
         <a href="/#/product/30">
-          <img v-lazy="'/imgs/banner-1.png'" alt="">
+          <img src="../../public/imgs/banner-1.png" alt />
         </a>
       </div>
-    </div>
-    <div class="product-box">
-      <div class="container">
-        <h2>手机</h2>
-        <div class="wrapper">
-          <div class="banner-left">
-            <a href="/#/product/35"><img v-lazy="'/imgs/mix-alpha.jpg'" alt=""></a>
-          </div>
-          <div class="list-box">
-            <div class="list" v-for="(arr,i) in phoneList" v-bind:key="i">
-              <div class="item" v-for="(item,j) in arr" v-bind:key="j">
-                <span v-bind:class="{'new-pro':j%2==0}">新品</span>
-                <div class="item-img">
-                  <img v-lazy="item.mainImage" alt="">
-                </div>
-                <div class="item-info">
-                  <h3>{{item.name}}</h3>
-                  <p>{{item.subtitle}}</p>
-                  <p class="price" @click="addCart(item.id)">{{item.price}}元</p>
+      <div class="product-box">
+        <div class="container">
+          <h2>手机</h2>
+          <div class="wrapper">
+            <div class="banner-left">
+              <a href="/#/product/35">
+                <img src="../../public/imgs/mix-alpha.jpg" alt />
+              </a>
+            </div>
+            <div class="list-box">
+              <div class="list" v-for="(arr,i) in phoneList" :key="i">
+                <div class="item" v-for="(item,j) in arr" :key="j">
+                  <span>新品</span>
+                  <div class="item-img">
+                    <img src="../../public/imgs/nav-img/nav-1.png" alt />
+                  </div>
+                  <div class="item-info">
+                    <h3>小米9</h3>
+                    <p>晓龙855+，索尼4800万超广角</p>
+                    <p class="price">2999元</p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      </div>-->
       <service-bar></service-bar>
       <!-- <modal 
       title="提示" 
@@ -187,24 +188,29 @@ export default {
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0]
+      ],
+      adsList: [
+        {
+          id: 33,
+          img: "/imgs/ads/ads-1.png"
+        },
+        {
+          id: 48,
+          img: "/imgs/ads/ads-2.jpg"
+        },
+        {
+          id: 45,
+          img: "/imgs/ads/ads-3.png"
+        },
+        {
+          id: 47,
+          img: "/imgs/ads/ads-4.jpg"
+        }
+      ],
+      phoneList: [
+        [1, 1, 1, 1],
+        [1, 1, 1, 1]
       ]
-      // adsList:[
-      //   {
-      //     id:33,
-      //     img:'/imgs/ads/ads-1.png'
-      //   },{
-      //     id:48,
-      //     img:'/imgs/ads/ads-2.jpg'
-      //   },{
-      //     id:45,
-      //     img:'/imgs/ads/ads-3.png'
-      //   },{
-      //     id:47,
-      //     img:'/imgs/ads/ads-4.jpg'
-      //   }
-      // ],
-      // phoneList:[],
-      // showModal:false
     };
   },
   mounted() {
@@ -322,99 +328,91 @@ export default {
         height: 100%;
       }
     }
-    // }
-    // .ads-box{
-    //   @include flex();
-    //   margin-top:14px;
-    //   margin-bottom:31px;
-    //   a{
-    //     width:296px;
-    //     height:167px;
-    //   }
-    // }
-    // .banner{
-    //   margin-bottom:50px;
-    // }
-    // .product-box{
-    //   background-color:$colorJ;
-    //   padding:30px 0 50px;
-    //   h2{
-    //     font-size:$fontF;
-    //     height:21px;
-    //     line-height:21px;
-    //     color:$colorB;
-    //     margin-bottom:20px;
-    //   }
-    //   .wrapper{
-    //     display:flex;
-    //     .banner-left{
-    //       margin-right:16px;
-    //       img{
-    //         width:224px;
-    //         height:619px;
-    //       }
-    //     }
-    // .list-box{
-    //   .list{
-    //     @include flex();
-    //     width:986px;
-    //     margin-bottom:14px;
-    //     &:last-child{
-    //       margin-bottom:0;
-    //     }
-    //     .item{
-    //       width:236px;
-    //       height:302px;
-    //       background-color:$colorG;
-    //       text-align:center;
-    //       span{
-    //         display:inline-block;
-    //         width:67px;
-    //         height:24px;
-    //         font-size:14px;
-    //         line-height:24px;
-    //         color:$colorG;
-    //         &.new-pro{
-    //           background-color:#7ECF68;
-    //         }
-    //         &.kill-pro{
-    //           background-color:#E82626;
-    //         }
-    //       }
-    //       .item-img{
-    //         img{
-    //           width:100%;
-    //           height:195px;
-    //         }
-    //       }
-    //       .item-info{
-    //         h3{
-    //           font-size:$fontJ;
-    //           color:$colorB;
-    //           line-height:$fontJ;
-    //           font-weight:bold;
-    //         }
-    //         p{
-    //           color:$colorD;
-    //           line-height:13px;
-    //           margin:6px auto 13px;
-    //         }
-    //         .price{
-    //           color:#F20A0A;
-    //           font-size:$fontJ;
-    //           font-weight:bold;
-    //           cursor:pointer;
-    //           &:after{
-    //             @include bgImg(22px,22px,'/imgs/icon-cart-hover.png');
-    //             content:' ';
-    //             margin-left:5px;
-    //             vertical-align: middle;
-    //           }
-    //         }
-    //       }
-    //     }
-    //   }
-    // }
+  }
+
+  .ads-box {
+    @include flex();
+    margin-top: 14px;
+    margin-bottom: 31px;
+    a {
+      width: 296px;
+      height: 167px;
+    }
+  }
+  .banner {
+    margin-bottom: 50px;
+  }
+  .product-box {
+    background-color: $colorJ;
+    padding: 30px 0 50px;
+    h2 {
+      font-size: 22px;
+      height: 21px;
+      line-height: 21px;
+      color: $colorB;
+      margin-bottom: 20px;
+    }
+    .wrapper {
+      display: flex;
+      .banner-left {
+        margin-right: 16px;
+        img {
+          width: 224px;
+          height: 100%;
+        }
+      }
+      .list-box {
+        .list {
+          @include flex();
+          width: 986px;
+          margin-bottom: 14px;
+          &:last-child {
+            margin-bottom: 0;
+          }
+          .item {
+            width: 236px;
+            height: 302px;
+            background-color: $colorG;
+            text-align: center;
+            .item-img {
+              img {
+                height: 195px;
+                margin-left: -15px;
+              }
+            }
+            .item-info {
+              h3 {
+                font-size: $fontJ;
+                color: $colorB;
+                line-height: $fontJ;
+                font-weight: bold;
+              }
+              p {
+                color: $colorD;
+                line-height: 13px;
+                margin: 6px auto 13px;
+              }
+              .price {
+                color: #f20a0a;
+                font-size: $fontJ;
+                font-weight: bold;
+                cursor: pointer;
+                &::after {
+                  @include bgImg(
+                    22px,
+                    22px,
+                    "../../public/imgs/icon-cart-hover.png"
+                  );
+                  content: "";
+                  margin-left: 5px;
+                  vertical-align: middle;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   }
 }
 </style>
